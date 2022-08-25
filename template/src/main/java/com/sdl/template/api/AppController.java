ae767ac4715b8d3047dba87e559db0e8b3bf7694
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-08-25 18:53:05 +0100

    user the response handler to display the age

--- template/src/main/java/com/sdl/template/api/AppController.java
+++ template/src/main/java/com/sdl/template/api/AppController.java
@@ -1,19 +1,37 @@
 package com.sdl.template.api;
 
+import com.sdl.template.dtos.ResponseHandler;
+import com.sdl.template.dtos.UserRequest;
+import com.sdl.template.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/")
+@RequestMapping("/")
 public class AppController {
-    
 
-    @GetMapping("howold")
-    public ResponseEntity<?> calculateAge(String dob) {
-        return new ResponseEntity<>("User Age", HttpStatus.OK);
+  private final UserService userService;
+
+  public AppController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("howold")
+  public ResponseEntity<Object> ageCalculator(@RequestBody UserRequest dob) {
+
+    try{
+      return ResponseHandler.apiResponse(HttpStatus.OK, true, "success",
+          userService.calculateAge(dob));
+    }catch (RuntimeException exception){
+      return ResponseHandler.apiResponse(HttpStatus.BAD_REQUEST, true, "fail",
+          exception.getLocalizedMessage());
     }
+
+  }
 
 }

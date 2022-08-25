7c5826da8fabf7e2fdcd39570de5c26eccc038f3
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-08-25 16:25:33 +0100

    date Util Added  unit test for the local date converter.

---
+++ template/src/test/java/com/sdl/template/utils/TimeUtilsTest.java
@@ -0,0 +1,23 @@
+package com.sdl.template.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.Month;
+import org.junit.jupiter.api.Test;
+
+class TimeUtilsTest {
+
+  @Test
+  public void calculate_localDate_from_dateTimeStamp(){
+    String date = "1990-07-10 13:27:27.16";
+    Timestamp timestamp = Timestamp.valueOf(date);
+    LocalDate expected = LocalDate.of(1990, Month.JULY, 10);
+    LocalDate actual = TimeUtils.dateFromTimeStamp(timestamp);
+    assertEquals(expected, actual);
+
+  }
+
+
+}

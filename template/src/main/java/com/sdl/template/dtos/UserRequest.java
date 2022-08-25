ea7d264c9be2320e2a618f40d7dd6eb47c5eec3c
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-08-25 18:51:26 +0100

    dto clean up and added tostring

--- template/src/main/java/com/sdl/template/dtos/UserRequest.java
+++ template/src/main/java/com/sdl/template/dtos/UserRequest.java
@@ -1,15 +1,22 @@
 package com.sdl.template.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import java.sql.Timestamp;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class UserRequest {
 
   @NotNull
+  @JsonFormat(shape= Shape.BINARY, pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
   private Timestamp dob;
+
+
 
 }
--- template/src/main/java/com/sdl/template/dtos/UserResponse.java
+++ template/src/main/java/com/sdl/template/dtos/UserResponse.java
@@ -3,9 +3,28 @@ package com.sdl.template.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
-@AllArgsConstructor
+
 public class UserResponse {
 
   private int years;
+
+  public UserResponse(int years) {
+    this.years = years;
+  }
+
+  public int getYears() {
+    return years;
+  }
+
+  public void setYears(int years) {
+    this.years = years;
+  }
+
+
+  @Override
+  public String toString() {
+    return "UserResponse{" +
+        years + "years" +
+        '}';
+  }
 }

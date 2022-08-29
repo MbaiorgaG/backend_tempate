79a3cf9e8bf4a3cdab7bfdcdf9ae38ab1e63bcd8
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-08-29 23:42:37 +0100

    added rate limiter to the controller

--- template/src/main/java/com/sdl/template/api/AppController.java
+++ template/src/main/java/com/sdl/template/api/AppController.java
@@ -1,37 +1,47 @@
 package com.sdl.template.api;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.sdl.template.dtos.ResponseHandler;
 import com.sdl.template.dtos.UserRequest;
 import com.sdl.template.services.UserService;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/")
 public class AppController {
-
+final Logger logger = LoggerFactory.getLogger(getClass());
   private final UserService userService;
 
-  public AppController(UserService userService) {
+  private final RateLimiter rateLimiter;
+
+  public AppController(UserService userService, RateLimiter rateLimiter) {
     this.userService = userService;
+    this.rateLimiter = rateLimiter;
   }
 
   @GetMapping("howold")
-  public ResponseEntity<Object> ageCalculator(@RequestBody UserRequest dob) {
-
-    try{
-      return ResponseHandler.apiResponse(HttpStatus.OK, true, "success",
-          userService.calculateAge(dob));
-    }catch (RuntimeException exception){
-      return ResponseHandler.apiResponse(HttpStatus.BAD_REQUEST, true, "fail",
-          exception.getLocalizedMessage());
+  public ResponseHandler ageCalculator(@RequestBody UserRequest dob, HttpServletRequest request) {
+    logger.info("request IP Address {}", request.getLocalPort());
+    boolean isOk = rateLimiter.tryAcquire();
+    if(isOk){
+      try {
+        return new ResponseHandler(HttpStatus.OK, false, "success",
+            userService.calculateAge(dob));
+      } catch (RuntimeException exception) {
+        return new ResponseHandler(HttpStatus.BAD_REQUEST, true, "Invalid request",
+            exception.getLocalizedMessage());
+      }
     }
-
+    else
+      return new ResponseHandler(HttpStatus.TOO_MANY_REQUESTS, true, "Only 1 request is allowed in 20 secs");
   }
 
 }
